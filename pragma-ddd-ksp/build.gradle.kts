8a6dc12b0b90a19180f2660fcb6eda0a78973422
morecup <morecup@163.com>
2025-08-20 17:22:20 +0800

    添加ksp依赖

--- pragma-ddd-ksp/build.gradle.kts
+++ pragma-ddd-ksp/build.gradle.kts
@@ -2,13 +2,13 @@ import org.gradle.kotlin.dsl.implementation
 
 plugins {
     id("kotlin-jvm")
-}
-
-repositories {
-    mavenCentral()
+    id("com.google.devtools.ksp") version "1.8.0-1.0.9"
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.8.0-1.0.9")
+
     testImplementation(kotlin("test"))
 }
 
